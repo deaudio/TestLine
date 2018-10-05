22a4d9f52f7ec1c0c2d5ac01dc677535c7ba18b8
deaudio <43780475+deaudio@users.noreply.github.com>
2018-10-05 15:26:25 +0700

    Update test.php

--- line-bot-exam-php-master/test.php
+++ line-bot-exam-php-master/test.php
@@ -5,11 +5,11 @@ require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 define("LINE_MESSAGING_API_CHANNEL_SECRET", 'f0c63c7170eab4b9f8027cf92c515a81');
 define("LINE_MESSAGING_API_CHANNEL_TOKEN", 'Gppzd6Y3lg6XHyA4wBZ0zWdyWmlqeDYBCxayHBYC8r8GnLXUSy2vJ9kN/NEdcdWqdoVJiZlgR3is39pmvR7OvjFEwGpMbheQ8cifL+ETAhZaz6DrrbVV1pT15YKjCXfhH0JI7sdaTS44/6vVtp1yLAdB04t89/1O/w1cDnyilFU=');
 
-use LINE\LINEBot;
-use LINE\LINEBot\HTTPClient\CurlHTTPClient;
-use LINE\LINEBot\Constant\HTTPHeader;
-use LINE\LINEBot\Event\MessageEvent;
-use LINE\LINEBot\Event\MessageEvent\TextMessage;
+use \LINE\LINEBot;
+use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use \LINE\LINEBot\Constant\HTTPHeader;
+use \LINE\LINEBot\Event\MessageEvent;
+use \LINE\LINEBot\Event\MessageEvent\TextMessage;
 
 $bot = new LINEBot(new CurlHTTPClient(LINE_MESSAGING_API_CHANNEL_TOKEN), [
             'channelSecret' => LINE_MESSAGING_API_CHANNEL_SECRET,
